1c470c6c9e64b36594496a373c889fb4ba7299eb
AntonioFialhoSN <antonio.fialho@discente.ufma.br>
2025-01-29 18:00:41 -0300

    COMMAND SEGUINDO O PADRÃO COM OS TIPOS ESPECIFICADOS NA ATIVIDADE 1

---
+++ Main/Command.py
@@ -0,0 +1,26 @@
+class Command:
+    class Type:
+        ADD = "ADD"
+        SUB = "SUB"
+        NEG = "NEG"
+        EQ = "EQ"
+        GT = "GT"
+        LT = "LT"
+        AND = "AND"
+        OR = "OR"
+        NOT = "NOT"
+        PUSH = "PUSH"
+        POP = "POP"
+
+    def __init__(self, command):
+        if command[0] == "if-goto":
+            self.type = Command.Type.IF
+        else:
+            self.type = getattr(Command.Type, command[0].upper(), None)
+
+        self.args = []
+        for arg in command[1:]:
+            pos = arg.find("//")
+            if pos != -1:
+                arg = arg[:pos]
+            self.args.append(arg.strip())
